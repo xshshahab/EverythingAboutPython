112d76201383378635f8038caab1aadb191dee5c
xshshahab <mdshahbuddin9504@gmail.com>
2024-12-27 21:09:48 +0530

    types of data types in python

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ I'm adding notes for each series so that you can easily navigate through the ser
 ### Series 1: Python Basics
 
 - **Day 1:** Introduction to Python
-- **Code and Notes :** [Python Basics Day 1](https://github.com/xshshahab/EverythingAboutPython/tree/main/01_Day)
+- **Code and Notes :** [Python Basics](https://github.com/xshshahab/EverythingAboutPython/tree/main/01_Day)
 
 - **Day 2:** Module and pip basic
 - **Code and Notes :** [Python Basics Modules and Pip Day 1](https://github.com/xshshahab/EverythingAboutPython/tree/main/02_Day)
+
+- **Day 3:** Variable and DataType
+- **Code and Notes :** [Python Basics Modules and Pip Day 2](https://github.com/xshshahab/EverythingAboutPython/tree/main/VariableDataType)
---
+++ VariableDataType/types.py
@@ -0,0 +1,30 @@
+# Creating a list
+fruits = ["apple", "banana", "cherry"]
+
+# Accessing items
+print(fruits[0])  # Output: apple
+print(fruits[1])  # Output: banana
+
+"""-------------------------------------------------------------------------------"""
+
+# Creating a tuple
+fruits = ("apple", "banana", "cherry")
+
+# Accessing items
+print(fruits[0])  # Output: apple
+print(fruits[1])  # Output: banana
+
+
+"""-------------------------------------------------------------------------------"""
+
+# Creating a dictionary
+person = {
+    "name": "xsh_shahab",
+    "age": 25,
+    "profession": "Software Engineer"
+}
+
+# Accessing values using keys
+print(person["name"])       # Output: Alice
+print(person["age"])        # Output: 25
+print(person["profession"]) # Output: Software Engineer
