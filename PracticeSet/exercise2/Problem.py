f877db88804e8a5ad74bab369aec8ff3209be267
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-02 00:51:16 +0530

    practice set 2

---
+++ PracticeSet/exercise2/Problem.py
@@ -0,0 +1,13 @@
+# import internal modules / built in modules
+
+import os 
+
+# specify the directory you want to list
+directory_path = "/home/username/Videos" # if you're using linux 
+
+# list all the files and directories in the specified path
+contents = os.listdir(directory_path)
+
+# print each file and directory name
+for eachfile in contents: 
+    print(contents)
---
+++ PracticeSet/exercise2/Problem1.py
@@ -0,0 +1,5 @@
+# Print multiple string using print and you can use single tripple quotes and tripple double quotes to print all the string in just using on print funciton.
+
+print("""\nTwinkle Twinkle little star
+how i wonder what you are
+""")
