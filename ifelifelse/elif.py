911518519aa43ec27aa19b44ecc3ac16dbf4abf8
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-04 22:02:01 +0530

    Learning if else elif

---
+++ ifelifelse/elif.py
@@ -0,0 +1,10 @@
+print("------------------- Learn Elif statements -------------------")
+
+num = int(input("Enter the value of a number : "))
+
+if(num < 0):
+    print("Number is negative")
+elif(num == 0): 
+    print("Number is zero")
+else :
+    print("Number is positive")
---
+++ ifelifelse/if-else.py
@@ -0,0 +1,8 @@
+applePrice = 210
+budget = 200
+
+if(applePrice <= budget) :
+    print("Syphar, add 1Kg Apples to the cart.")
+else :
+    print("Syphar, please don't add apples to the cart")
+    
