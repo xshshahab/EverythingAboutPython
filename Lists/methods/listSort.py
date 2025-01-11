5b4f129bcb3cc3f51b198da64af8397235968e7d
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-11 21:06:45 +0530

    sort method in list

---
+++ Lists/methods/listSort.py
@@ -0,0 +1,23 @@
+# Define a list of strings
+sortList = ["Vivek", "Ranjan", "Mohan", "Sita"]
+
+# Print the original list of strings
+print("Original list of strings:", sortList)
+
+# Sort the list of strings in ascending order (alphabetically)
+sortList.sort()
+
+# Print the sorted list of strings
+print("Sorted list of strings:", sortList)
+
+# Define a list of numbers
+numList = [45, 12, 89, 34, 7]
+
+# Print the original list of numbers
+print("Original list of numbers:", numList)
+
+# Sort the list of numbers in ascending order
+numList.sort()
+
+# Print the sorted list of numbers
+print("Sorted list of numbers:", numList)
