f18e74998df706cb3ecadbec64088ff320b5836a
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-11 21:11:07 +0530

    Reverse sort methods in python

---
+++ Lists/methods/reverse_sort.py
@@ -0,0 +1,11 @@
+# Define a list called 'num' with several integer values
+num = [1, 4, 4, 12, 43, 33, 7, 78, 6, 77, 9]
+
+# Print the original list of numbers
+print(num)
+
+# Sort the list in descending order by using the 'reverse=True' argument
+num.sort(reverse=True)
+
+# Print the sorted list after sorting in descending order
+print(num)
