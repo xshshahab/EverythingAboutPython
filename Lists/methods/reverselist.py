78e5b340a2194129f866fb9f5ead935eeaeebb97
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-11 21:13:03 +0530

    reverse() method learn in python

---
+++ Lists/methods/reverselist.py
@@ -0,0 +1,11 @@
+# Define a list called 'l' with string values representing fruit names
+l = ["Orange", "Papaya", "Coconut", "Banana"]
+
+# Print the original list of fruits
+print(l)
+
+# Reverse the order of elements in the list 'l'
+l.reverse()
+
+# Print the list after reversing the order of elements
+print(l)
