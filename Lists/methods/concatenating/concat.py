9018b4f936646b5cd692f66ed7d98fa2776e71cc
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-11 21:31:18 +0530

    concat strings in list using +

---
+++ Lists/methods/concatenating/concat.py
@@ -0,0 +1,11 @@
+# List of names
+names = ["Alice", "Bob", "Charlie"]
+
+# List of fruits
+fruits = ["Apple", "Banana", "Cherry"]
+
+# Concatenating the two lists
+combined_list = names + fruits
+
+# Printing the result
+print(combined_list)
