0d7f83658b6a2f7ef2582354cdec8555c93967ec
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-11 21:15:06 +0530

    index() method in list

---
+++ Lists/methods/indexList.py
@@ -0,0 +1,11 @@
+# Define a list of fruits
+fruits = ["Orange", "Papaya", "Coconut", "Banana", "Papaya"]
+
+# Print the original list of fruits
+print("Original list:", fruits)
+
+# Use the 'index()' method to find the index of the first occurrence of "Papaya"
+index_of_papaya = fruits.index("Papaya")
+
+# Print the index of the first occurrence of "Papaya"
+print("Index of 'Papaya':", index_of_papaya)
