aeb6e5d28f4fcedf02f21ec46bf8f91552e476ee
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-11 21:24:19 +0530

    insrt() method in list

---
+++ Lists/methods/insert/app.py
@@ -0,0 +1,11 @@
+# Define a list of numbers
+numbers = [10, 20, 30, 40]
+
+# Print the original list
+print("Original list of numbers:", numbers)
+
+# Insert a new number 25 at index 2
+numbers.insert(2, 25)
+
+# Print the list after insertion
+print("List after inserting 25 at index 2:", numbers)
---
+++ Lists/methods/insert/main.py
@@ -0,0 +1,11 @@
+# Define a list of fruits
+fruits = ["Apple", "Banana", "Cherry"]
+
+# Print the original list
+print("Original list:", fruits)
+
+# Use the insert() method to insert a new fruit at index 1
+fruits.insert(1, "Orange")
+
+# Print the list after insertion
+print("List after inserting 'Orange' at index 1:", fruits)
