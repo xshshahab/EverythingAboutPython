58aca5a9ec36caae7374d85e7663ca33b7a5f100
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-17 21:16:04 +0530

    Iteration in set

---
+++ 31_Set/code/iteration.py
@@ -0,0 +1,5 @@
+newset = {"Syphar", "Karlo", 222, 543, 102.333, "True", False, "Reflex", True, "False"}
+
+for eachSet in newset:
+    print(eachSet)
+    print(type(eachSet))
---
+++ 31_Set/code/main.py
@@ -0,0 +1,14 @@
+info = {"Syphar", 20, True, 20, 19.99}
+
+print(info)
+
+gojo = {}
+print(gojo)
+print(type(gojo))
+
+alez = set()
+print(alez)
+print(type(alez))
+
+for value in info:
+    print(value)
